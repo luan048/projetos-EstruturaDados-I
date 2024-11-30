fbde260205da29484e14a87fe270fbd108585746
luan048 <luanmonteiropedrosa@gmail.com>
2024-11-30 10:05:20 -0300

    Add commits in Histograma

--- HistogramaPilha/Histograma.java
+++ HistogramaPilha/Histograma.java
@@ -16,11 +16,14 @@ public class Histograma  {
         int elementoAtual = pilha.pop();
         int contador = 1;
 
+        // Enquanto pilha tiver valores
         while (!pilha.isEmpty()) {
-            if(elementoAtual == pilha.peek()) {
+            if(elementoAtual == pilha.peek()) { // Se o elementos atual é igual ao topo de pilha
                 contador++;
-                pilha.pop();
+                pilha.pop(); // Apaga elemento após adicionar +1 ao contador
             }
+            /*Se for diferente, vai imprimir a quantidade de vezes que o elementos anterior apareceu, vai apagar
+            * o topo da pilha, vai resetar contador = 1*/
             else {
                 System.out.println(elementoAtual + " -> " + contador + " vezes");
                 elementoAtual = pilha.pop();
@@ -28,8 +31,9 @@ public class Histograma  {
             }
         }
 
-        System.out.println(elementoAtual + " -> " + contador + " vezes");
+        System.out.println(elementoAtual + " -> " + contador + " vezes"); //Para listar o ultimo elemento
     }
+
     public static void main (String[] args) {
         Histograma temperatura = new Histograma();
 
